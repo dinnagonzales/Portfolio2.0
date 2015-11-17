7d49ac12ea029b66fca0e72e66c299b7eb476718
dinnagonzales <dinnagonzales.05@gmail.com>
2015-11-16 21:28:27 -0800

    Contact Form: Wufoo

--- src/javascript/components/contact.jsx
+++ src/javascript/components/contact.jsx
@@ -2,12 +2,20 @@
  * @jsx React.DOM
  */
 var React = require('react');
+var Iframe = require('react-iframe');
 
 var Contact = React.createClass({
   render: function() {
     return (
-      <div>
-        contact
+      <div className='row contact'>
+      	<div className='small-12 columns header'>
+      	contact
+  		</div>
+
+  		
+      	<div className='small-12 columns iframe'>
+      		<Iframe url='https://dinnagonzales.wufoo.com/forms/contact&amp;header=hide'/>
+      	</div>
       </div>
     );
   }
